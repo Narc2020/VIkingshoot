9bc0b56493d566a2be44f57575774cad3ba59301
Narc2020 <65845262+Narc2020@users.noreply.github.com>
2021-10-22 06:18:11 +0530

    all main

---
+++ Raw 20-10-2021/main2.js
@@ -0,0 +1,18 @@
+// $(function(){
+//             $('#value1, #value2').keyup(function(){
+//                var value1 = parseFloat($('#value1').val()) || 0;
+//                var value2 = parseFloat($('#value2').val()) || 0;
+//                $('#sum').val(value1 + value2);
+//             });
+//          });
+
+//  var value1 = parseFloat($('#value1').val()) || 0;
+//  var value2 = parseFloat($('#value2').val()) || 0;
+
+
+var a=parseFloat(document.getElementById("value1").value);
+var b=parseFloat(document.getElementById("value2").value);
+var sum = a+b;
+console.log(sum);
+
+
---
+++ Raw 20-10-2021/script.js
@@ -0,0 +1,4 @@
+var a=parseFloat(document.getElementById("value1").value);
+var b=parseFloat(document.getElementById("value2").value);
+var sum = a+b;
+console.log(sum);
